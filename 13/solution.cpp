37c6938345bc4ba015c6f742e1983a1394bb8b2c
xuzinan <xuzinan@bytedance.com>
2021-11-18 18:25:11 +0800

    add_13_solution
    Change-Id: I8bb3cb7a103d88e537732b6b7774df895dde58e4

---
+++ 13/solution.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+int ans[100];
+int main() {
+    string a;
+    while (cin >> a) {
+        reverse(a.begin(), a.end());
+        for (int i = 0; i < a.length(); i++) 
+            ans[i] += a[i] - '0';
+    }
+    for (int i = 0; i < 99; i++) {
+        ans[i + 1] += ans[i] / 10;
+        ans[i] %= 10;
+    }
+    int ind = 0;
+    for (int i = 99; i >= 0; i--)
+        if (ans[i]) {
+            ind = i;
+            break;
+        }
+    for (int i = ind; i > ind - 10; i--) cout << ans[i];
+    cout << endl;
+    return 0;
+}
