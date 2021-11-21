94c1be74d8eb1d1d4b0b159c833547606ee163bd
xuzinan <xuzinan@bytedance.com>
2021-11-21 20:26:56 +0800

    add_145_solution_README
    Change-Id: Ic7347588e848f475625f6db306206cef36c480ac

---
+++ 145/solution.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func rev(a int) int {
+	ret := 0
+	for a > 0 {
+		ret = ret * 10 + a % 10
+		a /= 10
+	}
+	return ret
+}
+
+func check(a int) int {
+	if a % 10 == 0 {
+		return 0
+	}
+	a += rev(a)
+	for a > 0 {
+		if a % 2 == 0 {
+			return 0
+		}
+		a /= 10
+	}
+	return 1
+}
+
+func main() {
+	upper := 1000 * 1000 * 100
+	ans := 0
+	for i := 1 ;i<=upper; i+=2 {
+		if check(i) == 1 {
+			ans++
+		}
+	}
+	fmt.Printf("%d\n", (ans<<1))
+}
