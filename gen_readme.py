d9b4b65897961e5ec5b884a5f01ff804853a7b6c
xuzinan <xuzinan@bytedance.com>
2021-11-21 20:17:59 +0800

    add_gen_readme
    Change-Id: Ia7ffca2973cc5454b1b4f5e3851f718ef0d2c87e

---
+++ gen_readme.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+
+ans = {}
+
+def init_ans():
+	with open("./answer.md", "r") as f:
+		anss = f.readlines()
+	for a in anss:
+		a = a[:len(a)-1]
+		sa = a.split(":")
+		ans[sa[0]] = sa[1]
+
+def handle(path):
+	if path not in ans:
+		print(path, " has no ans")
+		return 1/0
+	all_file_in_dir = os.listdir("./" + path)
+	read_me = ""
+	cpp = ""
+	go = ""
+	py = ""
+	for f in all_file_in_dir:
+		if f[len(f)-3:] == ".md":
+			read_me = f
+		elif f[len(f)-4:] == ".cpp":
+			cpp = f
+		elif f[len(f)-3:] == ".go":
+			go = f
+		elif f[len(f)-3:] == ".py":
+			py = f
+	if len(read_me) == 0:
+		print(path)
+		return 1/0
+	content = "|[" + str(path) + "](https://github.com/yydaily/project-euler-solution/blob/main/" + path + "/" + read_me + ")|"
+	if len(cpp) > 0:
+		content += "[c++](https://github.com/yydaily/project-euler-solution/blob/main/"+path+ "/" + cpp + ")"
+	content += "|"
+	if len(go) > 0:
+		content += "[go](https://github.com/yydaily/project-euler-solution/blob/main/"+path+ "/" + go + ")"
+	content += "|"
+	if len(py) > 0:
+		content += "[py](https://github.com/yydaily/project-euler-solution/blob/main/"+path+ "/" + py + ")"
+	content += "|"
+	content += ans[path]
+	content += "|"
+
+	with open("./README.md", "a") as f:
+		f.write(content + "\n")
+
+
+if __name__ == '__main__':
+	init_ans()
+	with open("./README.md", "w") as f:
+		f.write('''
+# Project Euler solutions
+
+
+欧拉计划一些简单题的程序和答案。
+我希望你能弄明白为什么是这样，而不是直接照搬答案。
+
+some answer for easy problem in project euler.
+I hope you can understand the principle instead of being a cheater.
+
+プロジェクトオイラーのプログラムと簡単な質問への回答。
+論理を理解していただければ幸いです，答えを直接コピーする代わりに。
+
+|Problem|c++|go|python|answer|
+|:------:|:------:|:------:|:------:|:------:|
+''')
+
+	all_file_and_dir = os.listdir(".")
+	need_handle = []
+	for f in all_file_and_dir:
+		if os.path.isfile(f): # filter file
+			continue
+		if f == ".git": # filter git
+			continue
+		need_handle.append(int(f))
+	for f in sorted(need_handle):
+		handle(str(f))
