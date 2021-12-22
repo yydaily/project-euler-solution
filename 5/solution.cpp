47b6f40e346e490f1f534627651fd897d6556f06
xuzinan <xuzinan@bytedance.com>
2021-12-22 20:48:07 +0800

    add_5_solution
    Change-Id: I9928450e4e8d90cb9f89ba3f19a31275d963b7a3

---
+++ 5/solution.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b) {
+	if(a==0) return b;
+	return gcd(b%a, a);
+}
+int lcm(int a, int b) {
+	return a/gcd(a, b)*b;
+}
+int main() {
+	int ans = 1;
+	for(int i = 2;i<=20;i++) ans = lcm(ans, i);
+	cout<<ans<<endl;
+	return 0;
+}
---
+++ 5/solution.py
@@ -0,0 +1,13 @@
+def gcd(a, b):
+	if a==0:
+		return b
+	return gcd(b%a, a)
+
+def lcm(a, b):
+	return a / gcd(a, b) * b
+
+ans = 1
+for i in range(2, 21):
+	ans = lcm(ans, i)
+
+print(ans)
