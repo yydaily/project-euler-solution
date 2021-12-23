d6094905b26b2705b29ad2a5ee6979b5558dae41
xuzinan <xuzinan@bytedance.com>
2021-12-23 15:22:11 +0800

    add_7_solution
    Change-Id: I4e4c740e1f15b364812b69472b1c773b0c3bebb3

---
+++ 7/solution.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+const int len = 1e6;
+bool prime[len];
+int main() {
+	int cnt = 0;
+	for(int i = 2;i<len;i++) {
+		if(!prime[i]) {
+			cnt++;
+			if(cnt==10001) cout<<i<<endl;
+			for(int j = i<<1;j<len;j+=i) prime[j] = true;
+		}
+	}
+	return 0;
+}
