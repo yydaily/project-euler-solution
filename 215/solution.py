024048d8a21967976b389785e111630e824a25ad
xuzinan <xuzinan@bytedance.com>
2021-12-24 15:38:09 +0800

    add_215_solution
    Change-Id: Idbf2b1c7bf13bb712a7dd8d5e6dd0f78f06af08e

---
+++ 215/solution.py
@@ -0,0 +1,54 @@
+ll = []
+l = []
+mat = []
+ans = []
+def generate(num2, num3):
+	global ll
+	global l
+	if num2 + num3 == 1:
+		a = 0
+		for i in ll:
+			a |= (1<<i)
+		l.append(a)
+		return
+	base = 0
+	if len(ll) != 0:
+		base = ll[len(ll)-1]
+	if num2 > 0:
+		ll.append(base+2)
+		generate(num2-1, num3)
+		ll = ll[:len(ll)-1]
+	if num3 > 0:
+		ll.append(base+3)
+		generate(num2, num3-1)
+		ll = ll[:len(ll)-1]
+
+def gen(buf):
+	ret = [0] * len(buf)
+	for i in range(len(buf)):
+		for j in mat[i]:
+			ret[i] += buf[j]
+	return ret
+
+if __name__ == '__main__':
+	h = 10
+	w = 32
+	for i in range(w%2, w, 2):
+		j = (w - i * 3) / 2
+		if j < 0:
+			break
+		generate(j, i)
+	for i in range(len(l)):
+		mat.append([])
+	for i in range(len(l)):
+		for j in range(i+1, len(l)):
+			if (l[i] & l[j]) == 0:
+				mat[i].append(j)
+				mat[j].append(i)
+	ans = [1] * len(l)
+	for i in range(1, h):
+		ans = gen(ans)
+	answer = 0
+	for i in ans:
+		answer += i
+	print(answer)
