18d5c4bbc93aca756ddb986b40999f9a5c22775a
xuzinan <xuzinan@bytedance.com>
2021-12-24 15:38:40 +0800

    add_215_solution
    Change-Id: I6a5945e95099abacd5641b44be366cb3be4ef135

--- 215/solution.go
+++ 215/solution.go
@@ -1,9 +1,12 @@
 package main
+
 import "fmt"
+
 var l = []int64{}
 var ll = []int{}
-func generate(num2, num3 int) {/*{{{*/
-	if(num2 + num3 == 1) {
+
+func generate(num2, num3 int) { /*{{{*/
+	if num2+num3 == 1 {
 		a := int64(0)
 		for _, i := range ll {
 			a |= (int64(1) << i)
@@ -25,38 +28,39 @@ func generate(num2, num3 int) {/*{{{*/
 		generate(num2, num3-1)
 		ll = ll[:len(ll)-1]
 	}
-}/*}}}*/
+} /*}}}*/
 var mat = [][]int{}
-func gen(a []int64) []int64 {/*{{{*/
+
+func gen(a []int64) []int64 { /*{{{*/
 	ret := make([]int64, len(a))
-	for i:=0;i<len(mat);i++ {
+	for i := 0; i < len(mat); i++ {
 		for _, j := range mat[i] {
 			ret[i] += a[j]
 		}
 	}
 	return ret
-}/*}}}*/
+} /*}}}*/
 func main() {
 	h := 10
 	w := 32
-	for i := w % 2;i*3 <= w;i+=2 {
-		j := (w - i * 3) / 2
+	for i := w % 2; i*3 <= w; i += 2 {
+		j := (w - i*3) / 2
 		generate(j, i)
 	}
 	mat = make([][]int, len(l))
 	for i := range l {
-		for j:=i+1;j<len(l);j++ {
+		for j := i + 1; j < len(l); j++ {
 			if (l[i] & l[j]) == 0 {
-				mat[i] = append(mat[i], j);
-				mat[j] = append(mat[j], i);
+				mat[i] = append(mat[i], j)
+				mat[j] = append(mat[j], i)
 			}
 		}
 	}
 	ans := make([]int64, len(l))
-	for i := 0;i<len(ans);i++ {
+	for i := 0; i < len(ans); i++ {
 		ans[i] = 1
 	}
-	for i:=1;i<h;i++ {
+	for i := 1; i < h; i++ {
 		ans = gen(ans)
 	}
 	answer := int64(0)
