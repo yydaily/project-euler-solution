b7cbb2a26090958ca9b341de20be39554937b82c
zinan.xu <zinan.xu@shopee.com>
2022-02-08 14:28:25 +0800

    add cpp gen

--- gen.sh
+++ gen.sh
@@ -16,20 +16,77 @@ cd $1
 
 # cpp
 touch solution.cpp
-echo "#include<iostream>
+echo "#include<iostream>/*{{{*/
 #include<vector>
 #include<set>
+#include<math.h>
 #include<map>
-using namespace std;
+const int e4 = 10000;
+const int e8 = e4*e4;
+const int mod = 1e9+7;
+using namespace std;/*}}}*/
+
+/*add{{{*/
+template <typename T>
+T base_add(T a, T b) {
+	a %= mod;
+	b %= mod;
+	return (a+b)%mod;
+}
+template <typename T>
+T add(const T &t) { return t; }
+template <typename T, typename ... Args>
+T add(const T &t, const Args&... args) { return base_add(t, add(args...)); }
+/*}}}*/
+
+/*{{{mul*/
+template <typename T>
+T base_mul(T a, T b) {
+	a%=mod;
+	b%=mod;
+	T ret(0);
+	while(b) {
+		if(b&1) ret = (ret + a) % mod;
+		b>>=1;
+		a = (a+a)%mod;
+	}
+	return ret;
+}
+template <typename T>
+T mul(const T &t) { return t; }
+template <typename T, typename ... Args>
+T mul(const T &t, const Args&... args) { return base_mul(t, mul(args...)); }
+/*}}}*/
+
+/*{{{qpow*/
+template <typename T>
+T qpow(T a, T b) {
+	T ret(1);
+	while(b) {
+		if(b&1) ret = mul(ret, a);
+		a = mul(a, a);
+		b>>=1;
+	}
+	return ret;
+}
+/*}}}*/
 int main() {
 	return 0;
 }" > solution.cpp
 
 # go
 touch solution.go
-echo "package main
+echo "package main /*{{{*/
 
-import \"fmt\"
+import (
+	\"fmt\"
+)
+
+const (
+	e4 = int64(10000)
+	e8 = e4*e4
+	mod = 1e9+7
+) /*}}}*/
 
 func main() {
 	fmt.Printf(\"1\")
