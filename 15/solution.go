c46d3abaf496c2c76db682172641b8f88d029541
zinan.xu <zinan.xu@shopee.com>
2022-02-21 16:35:43 +0800

    add 15 solution

---
+++ 15/solution.go
@@ -0,0 +1,83 @@
+package main /*{{{*/
+
+import (
+	"fmt"
+	"reflect"
+)
+
+const (
+	e4  = int64(10000)
+	e5  = int64(100000)
+	e6  = int64(1000000)
+	e7  = int64(10000000)
+	e8  = e4 * e4
+	mod = int64(1e9 + 7)
+)
+
+func add(list ...interface{}) int64 { /*{{{*/
+	if len(list) == 0 {
+		return 0
+	}
+	ret := int64(0)
+	for _, i := range list {
+		buf := int64(0)
+		switch reflect.TypeOf(i).String() {
+		case "int":
+			buf = int64(i.(int))
+		case "int64":
+			buf = i.(int64)
+		}
+		ret = (ret + buf) % mod
+	}
+	return ret
+} /*}}}*/
+func mul(list ...interface{}) int64 { /*{{{*/
+	if len(list) == 0 {
+		return 0
+	}
+	ret := int64(1)
+	for _, i := range list {
+		buf := int64(0)
+		switch reflect.TypeOf(i).String() {
+		case "int":
+			buf = int64(i.(int))
+		case "int64":
+			buf = i.(int64)
+		}
+		ret = (buf % mod * ret) % mod
+	}
+	return ret
+} /*}}}*/
+func qpow(a, b int64) int64 { /*{{{*/
+	ret := int64(1)
+	for b > 0 {
+		if (b & 1) == 1 {
+			ret = mul(ret, a)
+		}
+		a = mul(a, a)
+		b >>= 1
+	}
+	return ret
+} /*}}}*/
+/*}}}*/
+
+var c = [21][41]int64{}
+
+func cal(a, b int64) int64 {
+	if a > b {
+		return 0
+	}
+	if a == b || a == 0 {
+		return 1
+	}
+	if c[a][b] > 0 {
+		return c[a][b]
+	}
+	c[a][b] = cal(a-1, b-1) + cal(a, b-1)
+	return c[a][b]
+}
+
+func main() {
+	fmt.Println(cal(2, 4))
+	fmt.Println(cal(20, 40))
+}
