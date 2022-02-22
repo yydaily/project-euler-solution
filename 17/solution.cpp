d8b984409f014c1cf4bd38d492c87067e11dadc3
zinan.xu <zinan.xu@shopee.com>
2022-02-22 15:18:20 +0800

    add 17 solution

---
+++ 17/solution.cpp
@@ -0,0 +1,83 @@
+#include<iostream>/*{{{*/
+#include<vector>
+#include<set>
+#include<math.h>
+#include<map>
+#include "../template/bigint.cpp"
+const long long e2 = 100;
+const long long e3 = e2*10;
+const long long e4 = e3*10;
+const long long e5 = e4*10;
+const long long e6 = e4*e2;
+const long long e7 = e6*10;
+const long long e8 = e7*10;
+const long long mod = 1e9+7;
+using namespace std;/*}}}*/
+
+/*add{{{*/
+long long base_add(long long a, long long b) {
+	a %= mod;
+	b %= mod;
+	return (a+b)%mod;
+}
+long long add(initializer_list<long long> args) {
+	long long ret = 0;
+	for(auto i : args) ret = base_add(ret, i);
+	return ret;
+}
+/*}}}*/
+
+/*{{{mul*/
+long long base_mul(long long a, long long b) {
+	a%=mod;
+	b%=mod;
+	long long ret = 0;
+	while(b) {
+		if(b&1) ret = (ret + a) % mod;
+		b>>=1;
+		a = (a+a)%mod;
+	}
+	return ret;
+}
+long long mul(initializer_list<long long> args) {
+	long long ret = 1;
+	for(auto i : args) ret = base_mul(ret, i);
+	return ret;
+}/*}}}*/
+
+/*{{{qpow*/
+long long qpow(long long a, long long b) {
+	long long ret = 1;
+	while(b) {
+		if(b&1) ret = mul({ret, a});
+		a = mul({a, a});
+		b>>=1;
+	}
+	return ret;
+} /*}}}*/
+
+string num[30] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",  
+					"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty"};
+string num2[20] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+string cal(int a) {
+	int hundred = a/100;
+	int ten = a/10 - hundred*10;
+	int single = a%10;
+
+	string ret = "";
+	if(hundred>0) {
+		ret += num[hundred] + "Hundred"; // hundred
+		if(ten + single>0)  ret += "And"; // and
+	}
+	if(a%100<=20) return ret + num[a%100];
+	return ret + num2[ten] + num[single];
+}
+int main() {
+	int ans = 11; // one thousand
+	for(int i = 1;i<1000;i++) {
+		cout<<i<<' '<<cal(i)<<endl;
+		ans+=cal(i).length();
+	}
+	cout<<ans<<endl;
+	return 0;
+}
---
+++ 17/solution.go
@@ -0,0 +1,99 @@
+package main /*{{{*/
+
+import (
+	"fmt"
+	"reflect"
+)
+
+const (
+	e4  = int64(10000)
+	e5  = int64(100000)
+	e6  = int64(1000000)
+	e7  = int64(10000000)
+	e8  = e4 * e4
+	mod = int64(1e9 + 7)
+)
+
+func add(list ...interface{}) int64 { /*{{{*/
+	if len(list) == 0 {
+		return 0
+	}
+	ret := int64(0)
+	for _, i := range list {
+		buf := int64(0)
+		switch reflect.TypeOf(i).String() {
+		case "int":
+			buf = int64(i.(int))
+		case "int64":
+			buf = i.(int64)
+		}
+		ret = (ret + buf) % mod
+	}
+	return ret
+} /*}}}*/
+func mul(list ...interface{}) int64 { /*{{{*/
+	if len(list) == 0 {
+		return 0
+	}
+	ret := int64(1)
+	for _, i := range list {
+		buf := int64(0)
+		switch reflect.TypeOf(i).String() {
+		case "int":
+			buf = int64(i.(int))
+		case "int64":
+			buf = i.(int64)
+		}
+		buf %= mod
+		base := ret
+		ret = 0
+		for buf > 0 {
+			if (buf & 1) == 1 {
+				ret = add(ret, base)
+			}
+			base = add(base, base)
+			buf >>= 1
+		}
+	}
+	return ret
+} /*}}}*/
+func qpow(a, b int64) int64 { /*{{{*/
+	ret := int64(1)
+	for b > 0 {
+		if (b & 1) == 1 {
+			ret = mul(ret, a)
+		}
+		a = mul(a, a)
+		b >>= 1
+	}
+	return ret
+} /*}}}*/
+/*}}}*/
+
+var num = [30]string{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty"}
+var num2 = [20]string{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"}
+
+func cal(a int) string {
+	hundred := a / 100
+	ten := a/10 - hundred*10
+	single := a % 10
+
+	ret := ""
+	if hundred > 0 {
+		ret += num[hundred] + "Hundred"
+		if ten+single > 0 {
+			ret += "And"
+		}
+	}
+	if a%100 <= 20 {
+		return ret + num[a%100]
+	}
+	return ret + num2[ten] + num[single]
+}
+func main() {
+	ans := 11
+	for i := 1; i < 1000; i++ {
+		ans += len(cal(i))
+	}
+	fmt.Println(ans)
+}
