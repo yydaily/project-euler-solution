359fbc6d831fa066ae1e82577c438a3038d95e62
zinan.xu <zinan.xu@shopee.com>
2022-02-28 13:10:35 +0800

    add easy problem solution

---
+++ 47/solution.cpp
@@ -0,0 +1,91 @@
+#include<iostream>/*{{{*/
+#include<vector>
+#include<set>
+#include<math.h>
+#include<map>
+#include "../template/bigint.cpp"
+#include "../template/prime_cnt.cpp"
+#include "../template/china_reminder.cpp"
+#include "../template/euler.cpp"
+#include "../template/prime.cpp"
+const long long e2 = 100;
+const long long e3 = e2*10;
+const long long e4 = e3*10;
+const long long e5 = e4*10;
+const long long e6 = e4*e2;
+const long long e7 = e6*10;
+const long long e8 = e7*10;
+const long long mod = 1e9+7;
+using namespace std;/*}}}*/
+
+/*add{{{*/
+long long base_add(long long a, long long b) {
+	a %= mod;
+	b %= mod;
+	return (a+b)%mod;
+}
+long long add(long long a, long long b) {
+	return base_add(a, b);
+}
+long long add(initializer_list<long long> args) {
+	long long ret = 0;
+	for(auto i : args) ret = base_add(ret, i);
+	return ret;
+}
+/*}}}*/
+
+/*{{{mul*/
+long long base_mul(long long a, long long b) {
+	a%=mod;
+	b%=mod;
+	long long ret = 0;
+	while(b) {
+		if(b&1) ret = (ret + a) % mod;
+		b>>=1;
+		a = (a+a)%mod;
+	}
+	return ret;
+}
+long long mul(long long a, long long b) {
+	return base_mul(a, b);
+}
+long long mul(initializer_list<long long> args) {
+	long long ret = 1;
+	for(auto i : args) ret = base_mul(ret, i);
+	return ret;
+}/*}}}*/
+
+/*{{{qpow*/
+long long qpow(long long a, long long b) {
+	long long ret = 1;
+	while(b) {
+		if(b&1) ret = mul({ret, a});
+		a = mul({a, a});
+		b>>=1;
+	}
+	return ret;
+} /*}}}*/
+int cnt(int a) {
+	int ret = 0;
+	for(auto i : Prime::prime) {
+		if(i*i > a) break;
+		if(a%i==0) {
+			ret++;
+			while(a%i==0) a/=i;
+		}
+	}
+	return ret + (a!=1);
+}
+int main() {
+	int prime_cnt = 4;
+	Prime::init(e6);
+	for(int i = 2*3*5*7;;i++) {
+		bool ok = true;
+		for(int j = i;j<=i+prime_cnt-1;j++) if(cnt(j) != prime_cnt) ok = false;
+		if(ok) {
+			cout<<i<<endl;
+			break;
+		}
+	}
+	return 0;
+}
---
+++ 53/README.md
@@ -0,0 +1,14 @@
+## Combinatoric selections
+
+There are exactly ten ways of selecting three from five, $12345$:
+
+$123, 124, 125, 134, 135, 145, 234, 235, 245,$ and $345$
+
+In combinatorics, we use the notation, $\tbinom{5}{3} = 10$.
+
+In general, $\tbinom{n}{r} = \frac{n!}{r!(n-r)!}$, where $r\le n$, $n!=n\times(n-1) \times \dots\times 3\times 2\times 1$, and $0!=1$.
+
+It is not until $n=23$, that a value exceeds one-million: $\tbinom{23}{10}=1144066$.
+
+How many, not necessarily distinct, values of
+$\tbinom{n}{r}$ for $1\le n\le 100$, are greater than one-million?
