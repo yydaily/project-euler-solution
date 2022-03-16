95af3f1305bf0e9e6be50816f6fb02050af6100a
zinan.xu <zinan.xu@shopee.com>
2022-03-16 14:19:56 +0800

    add 100 solution

---
+++ code/100/solution.cpp
@@ -0,0 +1,90 @@
+#include<iostream>/*{{{*/
+#include<vector>
+#include<set>
+#include<math.h>
+#include<map>
+#include "../template/bigint.cpp"
+#include "../template/prime_cnt.cpp"
+#include "../template/china_reminder.cpp"
+#include "../template/euler.cpp"
+#include "../template/prime.cpp"
+#include "../template/pell.cpp"
+const long long e2 = 100;
+const long long e3 = e2*10;
+const long long e4 = e3*10;
+const long long e5 = e4*10;
+const long long e6 = e4*e2;
+const long long e7 = e6*10;
+const long long e8 = e7*10;
+long long mod = 1e9+7;
+using namespace std;/*}}}*/
+
+/*add{{{*/
+long long base_add(long long a, long long b) {
+	a %= mod;
+	b %= mod;
+	return (a+b)%mod;
+}
+long long add(long long a, long long b) {
+	return base_add(a, b);
+}
+long long add(initializer_list<long long> args) {
+	long long ret = 0;
+	for(auto i : args) ret = base_add(ret, i);
+	return ret;
+}
+/*}}}*/
+
+/*{{{mul*/
+long long base_mul(long long a, long long b) {
+	a%=mod;
+	b%=mod;
+	long long ret = 0;
+	while(b) {
+		if(b&1) ret = (ret + a) % mod;
+		b>>=1;
+		a = (a+a)%mod;
+	}
+	return ret;
+}
+long long mul(long long a, long long b) {
+	return base_mul(a, b);
+}
+long long mul(initializer_list<long long> args) {
+	long long ret = 1;
+	for(auto i : args) ret = base_mul(ret, i);
+	return ret;
+}/*}}}*/
+
+/*{{{qpow*/
+long long qpow(long long a, long long b) {
+	long long ret = 1;
+	while(b) {
+		if(b&1) ret = mul({ret, a});
+		a = mul({a, a});
+		b>>=1;
+	}
+	return ret;
+} /*}}}*/
+
+void gen(long long &a, long long &b) {
+	auto aa = -3*a -4*b + 3;
+	b = -2*a -3*b + 2;
+	a = aa;
+}
+int main() {
+	long long a = 0, b = 0;
+	long long va = 0, vb = 0;
+	while(vb<1e12) {
+		gen(a, b);
+		if(a<0) {
+			va = -a+1;
+			vb = 1-b*2;
+		} else {
+			va = a;
+			vb = b*2;
+		}
+		cout<<va<<' '<<vb<<endl;
+	}
+	return 0;
+}
