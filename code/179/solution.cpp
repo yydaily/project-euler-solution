a8c83803afb3d42c956b2e2e7582a2c907d858d2
zinan.xu <zinan.xu@shopee.com>
2022-03-17 14:38:54 +0800

    add 179 solution

---
+++ code/179/solution.cpp
@@ -0,0 +1,96 @@
+#include "../template/bigint.cpp"
+#include "../template/china_reminder.cpp"
+#include "../template/euler.cpp"
+#include "../template/pell.cpp"
+#include "../template/prime.cpp"
+#include "../template/prime_cnt.cpp"
+#include <iostream>/*{{{*/
+#include <map>
+#include <math.h>
+#include <set>
+#include <vector>
+const long long e2 = 100;
+const long long e3 = e2 * 10;
+const long long e4 = e3 * 10;
+const long long e5 = e4 * 10;
+const long long e6 = e4 * e2;
+const long long e7 = e6 * 10;
+const long long e8 = e7 * 10;
+long long mod      = 1e9 + 7;
+using namespace std; /*}}}*/
+
+/*add{{{*/
+long long base_add(long long a, long long b) {
+    a %= mod;
+    b %= mod;
+    return (a + b) % mod;
+}
+long long add(long long a, long long b) { return base_add(a, b); }
+long long add(initializer_list<long long> args) {
+    long long ret = 0;
+    for (auto i : args)
+        ret = base_add(ret, i);
+    return ret;
+}
+/*}}}*/
+
+/*{{{mul*/
+long long base_mul(long long a, long long b) {
+    a %= mod;
+    b %= mod;
+    long long ret = 0;
+    while (b) {
+        if (b & 1)
+            ret = (ret + a) % mod;
+        b >>= 1;
+        a = (a + a) % mod;
+    }
+    return ret;
+}
+long long mul(long long a, long long b) { return base_mul(a, b); }
+long long mul(initializer_list<long long> args) {
+    long long ret = 1;
+    for (auto i : args)
+        ret = base_mul(ret, i);
+    return ret;
+} /*}}}*/
+
+/*{{{qpow*/
+long long qpow(long long a, long long b) {
+    long long ret = 1;
+    while (b) {
+        if (b & 1)
+            ret = mul({ret, a});
+        a = mul({a, a});
+        b >>= 1;
+    }
+    return ret;
+} /*}}}*/
+
+int cal(int a) {
+    int ret = 1;
+    while (a != 1) {
+        int p   = Prime::max_prime[a];
+        int cnt = 0;
+        while (a % p == 0) {
+            a /= p;
+            cnt++;
+        }
+        ret *= (cnt + 1);
+    }
+    return ret;
+}
+int main() {
+    Prime::init(e7 + 1);
+    int ans  = 0;
+    int last = 1;
+    for (int i = 2; i <= e7; i++) {
+        int cnt = cal(i);
+        if (cnt == last) {
+            ans++;
+        }
+        last = cnt;
+    }
+    cout << ans << endl;
+    return 0;
+}
