4564036e307565b1f1d53902f4d369d52b3479bc
zinan.xu <zinan.xu@shopee.com>
2022-03-25 15:22:48 +0800

    add 136 solution

---
+++ code/136/solution.go
@@ -0,0 +1,95 @@
+package main /*{{{*/
+
+import (
+	"fmt"
+	"reflect"
+)
+
+const (
+	e4  = int64(10000)
+	e5  = int64(100000)
+	e6  = int64(1000000)
+	e7  = int64(10000000)
+	e8  = e4 * e4
+	mod = int64(1e9 + 7)
+)
+
+func add(list ...interface{}) int64 { /*{{{*/
+	if len(list) == 0 {
+		return 0
+	}
+	ret := int64(0)
+	for _, i := range list {
+		buf := int64(0)
+		switch reflect.TypeOf(i).String() {
+		case "int":
+			buf = int64(i.(int))
+		case "int64":
+			buf = i.(int64)
+		}
+		ret = (ret + buf) % mod
+	}
+	return ret
+} /*}}}*/
+func mul(list ...interface{}) int64 { /*{{{*/
+	if len(list) == 0 {
+		return 0
+	}
+	ret := int64(1)
+	for _, i := range list {
+		buf := int64(0)
+		switch reflect.TypeOf(i).String() {
+		case "int":
+			buf = int64(i.(int))
+		case "int64":
+			buf = i.(int64)
+		}
+		buf %= mod
+		base := ret
+		ret = 0
+		for buf > 0 {
+			if (buf & 1) == 1 {
+				ret = add(ret, base)
+			}
+			base = add(base, base)
+			buf >>= 1
+		}
+	}
+	return ret
+} /*}}}*/
+func qpow(a, b int64) int64 { /*{{{*/
+	ret := int64(1)
+	for b > 0 {
+		if (b & 1) == 1 {
+			ret = mul(ret, a)
+		}
+		a = mul(a, a)
+		b >>= 1
+	}
+	return ret
+} /*}}}*/
+/*}}}*/
+
+const upper = 100
+
+var cnt = [upper]int{}
+
+func main() {
+	for z := int64(1); z <= upper; z++ {
+		for d := z/3 + 1; ; d++ {
+			now := 2*z*d + 3*d*d - z*z
+			if now >= upper {
+				break
+			}
+			cnt[now]++
+		}
+	}
+	ans := 0
+	for _, i := range cnt {
+		fmt.Println(i)
+		if i == 1 {
+			ans++
+		}
+	}
+	fmt.Println(ans)
+}
