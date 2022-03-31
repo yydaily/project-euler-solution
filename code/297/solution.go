acd300ad7f5ed60b3ab9dc45c0d458531e8c0515
zinan.xu <zinan.xu@shopee.com>
2022-03-31 18:06:06 +0800

    add 297 solution

---
+++ code/297/solution.go
@@ -0,0 +1,108 @@
+package main /*{{{*/
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
+
+const (
+	e4  = int64(10000)
+	e5  = int64(100000)
+	e6  = int64(1000000)
+	e7  = int64(10000000)
+	e8  = e4 * e4
+	e9  = e4 * e5
+	mod = int64(1e9 + 7)
+)
+
+func sqrt(a int64) int64 {
+	x := math.Sqrt(float64(a))
+	return int64(x)
+}
+
+func add(list ...interface{}) int64 { /*{{{*/
+	if len(list) == 0 {
+		return 0
+	}
+	ret := int64(0)
+	for _, i := range list {
+		buf := int64(0)
+		switch reflect.TypeOf(i).String() {
+		case "int":
+			buf = int64(i.(int))
+		case "int64":
+			buf = i.(int64)
+		}
+		ret = (ret + buf) % mod
+	}
+	return ret
+} /*}}}*/
+func mul(list ...interface{}) int64 { /*{{{*/
+	if len(list) == 0 {
+		return 0
+	}
+	ret := int64(1)
+	for _, i := range list {
+		buf := int64(0)
+		switch reflect.TypeOf(i).String() {
+		case "int":
+			buf = int64(i.(int))
+		case "int64":
+			buf = i.(int64)
+		}
+		buf %= mod
+		base := ret
+		ret = 0
+		for buf > 0 {
+			if (buf & 1) == 1 {
+				ret = add(ret, base)
+			}
+			base = add(base, base)
+			buf >>= 1
+		}
+	}
+	return ret
+} /*}}}*/
+func qpow(a, b int64) int64 { /*{{{*/
+	ret := int64(1)
+	for b > 0 {
+		if (b & 1) == 1 {
+			ret = mul(ret, a)
+		}
+		a = mul(a, a)
+		b >>= 1
+	}
+	return ret
+} /*}}}*/
+/*}}}*/
+
+var num = []int64{}
+var m = map[int64]int64{}
+
+func cnt(a int64) int64 {
+	if a <= 0 {
+		return 0
+	}
+	if m[a] > 0 {
+		return m[a]
+	}
+	for i := len(num) - 1; i >= 0; i-- {
+		if a >= num[i] {
+			m[a] = 1 + cnt(num[i]-1) + cnt(a-num[i]) + a - num[i]
+			return m[a]
+		}
+	}
+	return 0
+}
+
+const upper = e8 * e9
+
+func main() {
+	a, b := int64(1), int64(1)
+	for b < upper {
+		num = append(num, b)
+		a, b = b, a+b
+	}
+	fmt.Println(cnt(upper - 1))
+}
