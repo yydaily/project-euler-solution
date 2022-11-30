6eb0671516877de5d8680d3b9e9c2cecf0f119e6
zinan.xu <zinan.xu@shopee.com>
2022-11-30 11:50:56 +0800

    update readme

--- gen_readme.py
+++ gen_readme.py
@@ -3,107 +3,86 @@
 import os
 
 ans = {}
-cnt_cpp = 0
-cnt_py = 0
-cnt_go = 0
-cnt_pdf = 0
+cnt = [0, 0, 0, 0] # cpp, py, go , pdf
 
-
+# 获取有答案的题目，全局变量ans会被初始化
 def init_ans():
     with open("./answer.md", "r") as f:
-        anss = f.readlines()
-    for a in anss:
-        a = a[: len(a) - 1]
-        if len(a) == 0:
+        ans_list = f.readlines()
+    for one_ans in ans_list:
+        one_ans = one_ans[: len(one_ans) - 1] # 去掉最后的回车
+        if len(one_ans) == 0: # 为空说明这个题目没有答案，直接过滤掉
             continue
-        sa = a.split(":")
-        if len(sa) != 2:
-            print(sa, " size is not 2")
-        ans[sa[0]] = sa[1]
+        index_and_ans = one_ans.split(":") # 按照冒号分割成题号和答案
+        if len(index_and_ans) != 2:
+            print(index_and_ans, " size is not 2")
+        ans[index_and_ans[0]] = index_and_ans[1]
 
 
+# 处理一个code下的子文件夹，用于生成read_me
 def handle(path):
-    global cnt_cpp, cnt_py, cnt_go, cnt_pdf
     if path not in ans:
         print(path, " has no ans")
         return 1 / 0
     all_file_in_dir = os.listdir("./code/" + path)
-    read_me = ""
-    cpp = ""
-    go = ""
-    py = ""
-    pdf = ""
+    file_paths = ["", "", "", "", ""] # read_me, cpp, go, py, pdf
+
+    def handle_suffix(file_name, suffix, index):
+        global cnt
+        if file_name.endswith(suffix):
+            file_paths[index] = file_name
+            if index > 0:
+                cnt[index-1] += 1
+
     for f in all_file_in_dir:
-        if f[len(f) - 3 :] == ".md":
-            read_me = f
-        elif f[len(f) - 4 :] == ".cpp":
-            cnt_cpp += 1
-            cpp = f
-        elif f[len(f) - 3 :] == ".go":
-            cnt_go += 1
-            go = f
-        elif f[len(f) - 3 :] == ".py":
-            cnt_py += 1
-            py = f
-        elif f[len(f) - 4 :] == ".pdf":
-            cnt_pdf += 1
-            pdf = f
-    if len(read_me) == 0:
-        print(path)
+        handle_suffix(f, ".md", 0)
+        handle_suffix(f, ".cpp", 1)
+        handle_suffix(f, ".go", 2)
+        handle_suffix(f, ".py", 3)
+        handle_suffix(f, ".pdf", 4)
+    if len(file_paths[0]) == 0:
+        print(path, " has no read_me")
         return 1 / 0
-    content = (
-        "|["
-        + str(path)
-        + "](https://github.com/yydaily/project-euler-solution/tree/master/code/"
-        + path
-        + "/"
-        + read_me
-        + ")|"
-    )
-    if len(cpp) > 0:
-        content += (
-            "[c++](https://github.com/yydaily/project-euler-solution/tree/master/code/"
-            + path
-            + "/"
-            + cpp
-            + ")"
-        )
-    content += "|"
-    if len(go) > 0:
-        content += (
-            "[go](https://github.com/yydaily/project-euler-solution/tree/master/code/"
-            + path
-            + "/"
-            + go
-            + ")"
-        )
-    content += "|"
-    if len(py) > 0:
-        content += (
-            "[py](https://github.com/yydaily/project-euler-solution/tree/master/code/"
-            + path
-            + "/"
-            + py
-            + ")"
-        )
-    content += "|"
-    if len(pdf) > 0:
-        content += (
-            "[pdf](https://github.com/yydaily/project-euler-solution/tree/master/code/"
-            + path
-            + "/"
-            + pdf
-            + ")"
-        )
-    content += "|"
+
+    def gen_content(show, file_name):
+        if len(file_name) == 0:
+            return "|"
+        return "[" + show + "](https://github.com/yydaily/project-euler-solution/tree/master/code/" + path + "/" + file_name + ")|"
+    content = "|"
+    content += gen_content(str(path), file_paths[0])
+    content += gen_content("c++", file_paths[0])
+    content += gen_content("go", file_paths[0])
+    content += gen_content("py", file_paths[0])
+    content += gen_content("pdf", file_paths[0])
     content += ans[path]
     content += "|"
 
-    with open("./README.md", "a") as f:
-        f.write(content + "\n")
+    return content
 
 
 def write_prefix(problem_cnt):
+    def gen_ans_table():
+        max_cnt = 0
+        for k in ans:
+            max_cnt = max(max_cnt, int(k))
+        ret = "<table>"
+        for i in range(1, max_cnt+1):
+            if i % 10 == 1:
+                ret += "\t<tr>"
+
+            color = "#AFAFAF"
+            index = str(i)
+            if str(i) in ans:
+                color = "#30C705"
+                index = "<a href=https://github.com/yydaily/project-euler-solution/tree/master/code/"+index+">"+index+"</a>"
+            ret += "\t\t<td bgcolor=" + color + "><center>" + index + "</center></td>\n"
+
+            if i % 10 == 0 or i == max_cnt:
+                ret += "\t</tr>\n"
+        ret += "</table>\n"
+        return ret
+
+
     with open("./README.md", "w") as f:
         f.write(
             """
@@ -119,48 +98,36 @@ I hope you can understand the principle instead of being a cheater.
 プロジェクトオイラーのプログラムと簡単な質問への回答。
 論理を理解していただければ幸いです，答えを直接コピーする代わりに。
 
+{ans_table}
+
 |Problem|c++|go|python|pdf|answer|
 |:------:|:------:|:------:|:------:|:------:|:------:|
 """.format(
+                ans_table=gen_ans_table(),
                 problem_cnt=problem_cnt
             )
         )
 
+def show_cnt(total_cnt):
+    print("Problem cnt=" + str(total_cnt))
+    name_list = ["cpp", "go", "py", "pdf"]
+    for index in range(4):
+        print name_list[index] + " file cnt=" + str(cnt[index]) + " rate=" + str(cnt[index] * 100.0 / total_cnt) + "%"
 
 if __name__ == "__main__":
     init_ans()
     all_file_and_dir = os.listdir("./code")
     need_handle = []
+    filter_files = ["template", "gen.sh"]
     for f in all_file_and_dir:
-        if os.path.isfile(f):  # filter file
-            continue
-        if f == "template":  # filter template
-            continue
-        if f == "gen.sh":  # filter gen.sh
+        if os.path.isfile(f) or f in filter_files:  # filter file
             continue
         need_handle.append(int(f))
     cnt_prob = len(need_handle)
     write_prefix(cnt_prob)
+    content = ""
     for f in sorted(need_handle):
-        handle(str(f))
-    print("Problem cnt=" + str(cnt_prob))
-    print(
-        "cpp file cnt="
-        + str(cnt_cpp)
-        + " rate="
-        + str(cnt_cpp * 100.0 / cnt_prob)
-        + "%"
-    )
-    print(
-        "go file cnt=" + str(cnt_go) + " rate=" + str(cnt_go * 100.0 / cnt_prob) + "%"
-    )
-    print(
-        "py file cnt=" + str(cnt_py) + " rate=" + str(cnt_py * 100.0 / cnt_prob) + "%"
-    )
-    print(
-        "pdf file cnt="
-        + str(cnt_pdf)
-        + " rate="
-        + str(cnt_pdf * 100.0 / cnt_prob)
-        + "%"
-    )
+        content += handle(str(f))
+    with open("./README.md", "a") as f:
+        f.write(content + "\n")
+    show_cnt(cnt_prob)
